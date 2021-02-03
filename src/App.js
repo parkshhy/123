6723ef47e2ba0dbd3163d59dbbdcb55212e8bc9f
parkshhy <rainbleubird8@gmail.com>
2021-02-03 15:37:12 +0900

    1

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           <Header className="header-color" title={<Link style={{
             textDecoration: 'none',
             color: "white"
-          }} to="/"><strong>My1Portfolio</strong></Link>} scroll>
+          }} to="/"><strong>My12Portfolio</strong></Link>} scroll>
             <Navigation >
               <Link to="/resume"><strong>Resume/Contect</strong></Link>
               <Link to="/aboutme"><strong>AboutMe</strong></Link>
