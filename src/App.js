f320010039748aea1ef372328370b56d81e1359a
parkshhy <rainbleubird8@gmail.com>
2021-02-03 15:31:21 +0900

    1

--- src/App.js
+++ src/App.js
@@ -15,19 +15,19 @@ class App extends Component {
             color: "white"
           }} to="/123"><strong>MyPortfolio</strong></Link>} scroll>
             <Navigation >
-              <Link to="123/resume"><strong>Resume/Contect</strong></Link>
-              <Link to="123/aboutme"><strong>AboutMe</strong></Link>
-              <Link to="123/projects"><strong>Projects</strong></Link>
+              <Link to="/resume"><strong>Resume/Contect</strong></Link>
+              <Link to="/aboutme"><strong>AboutMe</strong></Link>
+              <Link to="/projects"><strong>Projects</strong></Link>
             </Navigation>
           </Header>
           <Drawer className="navi-grid" title={<Link style={{
             textDecoration: 'none',
             color: "black"
-          }} to="/123"><strong>MyPortfolio</strong></Link>}>
+          }} to="/"><strong>MyPortfolio</strong></Link>}>
             <Navigation>
-              <Link to="123/resume" ><strong>Resume/Contect</strong></Link>
-              <Link to="123/aboutme"><strong>AboutMe</strong></Link>
-              <Link to="123/projects"><strong>Projects</strong></Link>
+              <Link to="/resume" ><strong>Resume/Contect</strong></Link>
+              <Link to="/aboutme"><strong>AboutMe</strong></Link>
+              <Link to="/projects"><strong>Projects</strong></Link>
             </Navigation>
           </Drawer>
           <Content className="navi-grid">
